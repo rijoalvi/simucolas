fafd780ecb18e70516a06171c5448bdbaefe76e0
luiscarlosch <luiscarlosch@17ad76f6-ef9b-6add-ae07-a25734873f87>
2010-07-13 17:11:49 +0000

    Código documentado mejor

--- trunk/SimSistemaColasSinPrioridadAdquirida/Cliente.cs
+++ trunk/SimSistemaColasSinPrioridadAdquirida/Cliente.cs
@@ -6,6 +6,9 @@ using System.Windows.Forms;
 
 namespace SimSistemaColasSinPrioridadAdquirida
 {
+    /// <summary>
+    /// Representa un cliente en el sistema. Con tiene los datos propios de su hora de llegada, hora de salida, el tiempo que espero en la cola, el tipo de cliente que es, y un identificador único.
+    /// </summary>
     public class Cliente
     {
         public int IDGeneral;
@@ -20,6 +23,10 @@ namespace SimSistemaColasSinPrioridadAdquirida
             this.tipo = tipo;
             this.TMInicio = TMInicio;
         }
+        /// <summary>
+        /// Establece el tiempo que el cliente salió de la cola, y calcula el tiempo que estuvo en la cola
+        /// </summary>
+        /// <param name="TMFinal">The TM final.</param>
         public void setTMFinal(Double TMFinal) { 
             this.TMFinal=TMFinal;
             this.tiempoEsperaCola = TMFinal - TMInicio;
--- trunk/SimSistemaColasSinPrioridadAdquirida/MiliSegundo.cs
+++ trunk/SimSistemaColasSinPrioridadAdquirida/MiliSegundo.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace SimSistemaColasSinPrioridadAdquirida
 {
+    /// <summary>
+    /// Es un elemento que usamos para grabar el estado en un instante del sistema. Contiene el milisegundo que marca el sistema, el estado de la cola, y el estado del servidor.  Es utilizado por el estadisticador para llevar el control de los eventos en el sistema.
+    /// </summary>
     public class MiliSegundo
     {
         public int TM;
